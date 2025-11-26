10f0b4d77b491e206f36fe506801228eef0d68a0
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-26 17:18:27 +0530

    Added new features

--- src/Personallife.jsx
+++ src/Personallife.jsx
@@ -6,6 +6,7 @@ import sanna from "/sanna.png";
 import lovelyimg from "/lovely.png";
 import ckhouse from "/ckhouse.jpg";
 import sagardighi from "/sagardighi.jpg";
+import bangalore from "/blr.jpg";
 
 function Personallife() {
   return (
@@ -93,6 +94,40 @@ function Personallife() {
       </div>
       <div className="my-city">
         <img src={sagardighi} className="my-city-img" />
+      </div>
+      <div className="divider">
+        <h1>About Bangalore</h1>
+      </div>
+      <div className="about-blr-div">
+        <div className="about-blr-details">
+          <div className="bangalore-poster">
+            <img
+              src={bangalore}
+              alt="bangalore"
+              className="bangalore-poster-img"
+            />
+          </div>
+          <p>
+            I first came to Bangalore back in 2012 or 2013, when I was in VIT,
+            to a school-friends place, I actually started smoking weed for the
+            first time from there. Then I returned to Bangalore in 2017, after
+            finishing VIT, this time to search jobs. Bangalore gave me a lot of
+            jobs, one after another, marathon of jobs, this city gave me
+            Identity, a chance to grow, my first metro experience, but it also
+            maximized my addiction. I never faced any kind of security and
+            safety issues in Bangalore, people are nice, weather is nice, good
+            food, the city is expensive and very large and also clean. it's been
+            almost 9 years here in Bangalore for me, and I was unable to build
+            any career and job due to my addiction, but Bangalore gave me one
+            chance after another, I thank all the Job-Consultancy people who
+            helped me find jobs and thanks to all the companies which gave me so
+            many opportunities, and I am not done yet, I hope Bangalore will
+            keep helping me even now even in the future. Whatever I am today
+            truly, is because of Bangalore. Right now at age 33, I am looking
+            for long-term stability, and I hope Bangalore will help me find
+            that.
+          </p>
+        </div>
       </div>
     </>
   );
